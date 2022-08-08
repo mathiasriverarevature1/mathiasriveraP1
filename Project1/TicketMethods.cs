2e5e05c88372bce9cd35acdcc5c9d27210b4e828
DESKTOP-8EBH9SK <mathias750@revature.net>
2022-08-08 16:03:29 -0500

    More commented planning

---
+++ Project1/TicketMethods.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    public class TicketMethods
+    {
+
+        //Create ticket method with amount and description
+
+        //view ticket method
+        //ticket filter method
+
+        //approve or deny method. after approved or denied they cannot be changed again
+
+
+        
+    }
+}
