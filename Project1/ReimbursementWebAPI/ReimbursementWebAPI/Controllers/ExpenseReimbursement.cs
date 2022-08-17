a71f0f7260d8b39e87d10562f4b60eba858f2063
DESKTOP-8EBH9SK <mathias750@revature.net>
2022-08-17 09:28:50 -0500

    half way through mark's step 4 tutorial

---
+++ Project1/ReimbursementWebAPI/ReimbursementWebAPI/Controllers/ExpenseReimbursement.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace ReimbursementWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpenseReimbursement : ControllerBase
+    {
+        [HttpGet("PendingTickets")]
+        public async ActionResult<List<Ticket>> PendingRequests()
+        {
+            if(Type.Equals("pending")
+                {
+                List<Ticket> ticketList = new List<Ticket>();
+                return ticket;
+                }
+        }
+    }//EOC
+}//EON
